966bab7bc3da081a9a34aa4c51ee4674f3070fb4
Aaron <troublor@live.com>
2020-11-29 02:44:53 +0800

    send tx trace more precise

--- packages/augur-contract-dependencies-ethers/src/ContractDependenciesEthers.ts
+++ packages/augur-contract-dependencies-ethers/src/ContractDependenciesEthers.ts
@@ -264,11 +264,36 @@ export class ContractDependenciesEthers implements Dependencies<BigNumber> {
     // the 'from field is required to estimate gas but will fail if present when the transaction is sent.
     delete tx.from;
 
+// TODO troublor modify starts: record stack trace
+    const traceObj = {stack: undefined}
+    Error.captureStackTrace(traceObj, this.submitTransaction)
+    // troublor modify ends
+
     const response = await this.signer.sendTransaction({
       ...tx,
       gasLimit,
     });
     const hash = response.hash;
+
+    // TODO troublor modify starts: send trace via websocket
+    const trace = {
+      // ts-ignore
+      hash: hash,
+      stack: traceObj.stack.split(/\n/).map(item => item.trim()).filter(item => item.length > 0 && item !== "Error"),
+    };
+    console.log("Transaction trace", {hash: trace.hash, stack: trace.stack})
+    const ws = new WebSocket(`ws://localhost:1236`);
+    ws.onopen = () => {
+      ws.send(JSON.stringify(trace));
+    };
+    ws.onerror = (ev: ErrorEvent) => {
+      console.error("Send transaction trace error:", ev);
+    }
+    ws.onmessage = () => {
+      ws.close();
+    }
+    // troublor modify ends
+
     this.onTransactionStatusChanged(
       txMetadata,
       TransactionStatus.PENDING,
